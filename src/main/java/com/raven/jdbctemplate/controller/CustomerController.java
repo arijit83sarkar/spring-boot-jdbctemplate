fcfffea877ab7cf52c7e84d379d0a2e8b44c6015
arijit83sarkar <mail2arijit83@gmail.com>
2022-08-03 01:12:21 +0530

    customer controller modified

--- src/main/java/com/raven/jdbctemplate/controller/CustomerController.java
+++ src/main/java/com/raven/jdbctemplate/controller/CustomerController.java
@@ -18,123 +18,96 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 4800, allowCredentials = "false", methods = {
+		RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE })
+
 @RestController
 @RequestMapping("/api/customer")
 @Tag(description = "APIs related with Customers", name = "Customer")
 public class CustomerController {
 
-    private CustomerJDBCRepository customerJDBCRepository;
+	private CustomerJDBCRepository customerJDBCRepository;
 
-    @Autowired
-    public CustomerController(CustomerJDBCRepository customerJDBCRepository) {
-        this.customerJDBCRepository = customerJDBCRepository;
-    }
+	@Autowired
+	public CustomerController(CustomerJDBCRepository customerJDBCRepository) {
+		this.customerJDBCRepository = customerJDBCRepository;
+	}
 
-    @Operation(summary = "Total record count",
-            description = "Get total Customer count")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}",
-                    content = {@Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class)))}),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/getRecordNumber", method = RequestMethod.GET)
-    @ResponseBody
-    public int getRecordNumber() {
-        return customerJDBCRepository.count();
-    }
+	@Operation(summary = "Total record count", description = "Get total Customer count")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class))) }),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/getRecordNumber", method = RequestMethod.GET)
+	@ResponseBody
+	public int getRecordNumber() {
+		return customerJDBCRepository.count();
+	}
 
-    @Operation(summary = "All customer",
-            description = "Get all customer details")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}",
-                    content = {@Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class)))}),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/getAllCustomer", method = RequestMethod.GET)
-    @ResponseBody
-    public List<CustomerModel> getAllCustomer() {
-        return customerJDBCRepository.findAll();
-    }
+	@Operation(summary = "All customer", description = "Get all customer details")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class))) }),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/getAllCustomer", method = RequestMethod.GET)
+	@ResponseBody
+	public List<CustomerModel> getAllCustomer() {
+		return customerJDBCRepository.findAll();
+	}
 
-    @Operation(summary = "Customer details by customer number",
-            description = "Customer details by customer number")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}",
-                    content = {@Content(mediaType = "application/json",
-                            array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class)))}),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public Optional<CustomerModel> getCustomerByNumber(@PathVariable("id") int id) {
-        return customerJDBCRepository.findByCustomerNumber(id);
-    }
+	@Operation(summary = "Customer details by customer number", description = "Customer details by customer number")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}", content = {
+					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CustomerModel.class))) }),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public Optional<CustomerModel> getCustomerByNumber(@PathVariable("id") int id) {
+		return customerJDBCRepository.findByCustomerNumber(id);
+	}
 
-    @Operation(summary = "Save customer details",
-            description = "Save customer details")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}"),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    @ResponseBody
-    public int save(@Valid @RequestBody CustomerModel customerModel) {
-        return customerJDBCRepository.saveCustomer(customerModel);
-    }
+	@Operation(summary = "Save customer details", description = "Save customer details")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	@ResponseBody
+	public int save(@Valid @RequestBody CustomerModel customerModel) {
+		return customerJDBCRepository.saveCustomer(customerModel);
+	}
 
-    @Operation(summary = "Update customer details",
-            description = "Update customer details using customer number")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}"),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
-    @ResponseBody
-    public int update(@Valid @RequestBody CustomerModel customerModel, @PathVariable("id") int id) {
-        return customerJDBCRepository.updateCustomer(customerModel, id);
-    }
+	@Operation(summary = "Update customer details", description = "Update customer details using customer number")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+	@ResponseBody
+	public int update(@Valid @RequestBody CustomerModel customerModel, @PathVariable("id") int id) {
+		return customerJDBCRepository.updateCustomer(customerModel, id);
+	}
 
-    @Operation(summary = "Delete a customer",
-            description = "Delete a customer using customer number")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",
-                    description = "${api.response-codes.ok.desc}"),
-            @ApiResponse(responseCode = "400",
-                    description = "${api.response-codes.badRequest.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})}),
-            @ApiResponse(responseCode = "404",
-                    description = "${api.response-codes.notFound.desc}",
-                    content = {@Content(examples = {@ExampleObject(value = "")})})})
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
-    public int delete(@PathVariable("id") int id) {
-        return customerJDBCRepository.deleteCustomer(id);
-    }
+	@Operation(summary = "Delete a customer", description = "Delete a customer using customer number")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+			@ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }),
+			@ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}", content = {
+					@Content(examples = { @ExampleObject(value = "") }) }) })
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	public int delete(@PathVariable("id") int id) {
+		return customerJDBCRepository.deleteCustomer(id);
+	}
 }
